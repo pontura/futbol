16d4f6f10b044c294b54d3845bd089663952562f
Agustin Pontura <pontura@gmail.com>
2020-12-16 20:16:49 -0300

    goles desde los carteles

---
+++ Assets/GoalAsset.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalAsset : MonoBehaviour
+{
+    public GameObject goalAsset;
+    public GameObject notGoalAsset;
+    public GameObject[] teams;
+
+    void Start()
+    {
+        Loop();
+        Events.OnGoal += OnGoal;
+    }
+    void Loop()
+    {
+        notGoalAsset.SetActive(true);
+        goalAsset.SetActive(false);
+    }
+    void OnDestroy()
+    {
+        Events.OnGoal -= OnGoal;
+    }
+    void OnGoal(int teamID, Character character)
+    {
+        notGoalAsset.SetActive(false);
+        goalAsset.SetActive(true);
+
+        foreach(GameObject go in teams)
+            go.SetActive(false);
+
+        teams[character.teamID-1].SetActive(true);
+
+        Invoke("Loop", 5);
+    }
+    
+}
