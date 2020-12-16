f4df2ab6c60662be8d1d01d35b2971ed496f8cae
Agustin Pontura <pontura@gmail.com>
2020-12-16 11:29:24 -0300

    con controles editables

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class MainMenu : MonoBehaviour
 {
+    public Rewired.UI.ControlMapper.ControlMapper controlMapper_to_add;
+    Rewired.UI.ControlMapper.ControlMapper controlMapper;
+    public Canvas controlMappingCanvas;
+
     void Start()
     {
         Events.PlaySound("crowd", "fulbo_music1", true);
         Events.OnButtonClick += OnButtonClick;
+        controlMapper = Instantiate( controlMapper_to_add);
+        controlMapper.rewiredInputManager = GameObject.Find("Rewired Input Manager(Clone)").GetComponent< Rewired.InputManager>();
+        controlMappingCanvas = controlMapper.GetComponentInChildren<Canvas>();
     }
     void OnDestroy()
     {
@@ -15,10 +23,16 @@ public class MainMenu : MonoBehaviour
     }
     void OnButtonClick(int buttonID, int playerID)
     {
+        if (controlMapper.isOpen)
+            return;
         GotoGame();
     }
     public void GotoGame()
     {
         Data.Instance.LoadLevel("Selector");
+    }
+    public void Controls()
+    {
+        controlMapper.Open();
     }
 }
