7704a58fe769f000df9b0241e6b704d3feffe560
Agustin Pontura <pontura@gmail.com>
2021-04-29 18:18:05 -0300

    PC version

--- Assets/BallAI.cs
+++ Assets/BallAI.cs
@@ -24,7 +24,6 @@ public class BallAI : MonoBehaviour
     public void Reset()
     {
         CancelInvoke();
-        print("Reset_____________");
         state = states.IDLE;
     }
     void Loop()
